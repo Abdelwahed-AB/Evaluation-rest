18dfe8e4bcf4acfc5a4303841c67c191590965d0
Abdelwahed AB <ababdelwahed112@gmail.com>
2024-03-13 09:16:02 +0000

    initial

---
+++ src/main/java/com/evaluationrest/evaluationrest/EvaluationRestApplication.java
@@ -0,0 +1,13 @@
+package com.evaluationrest.evaluationrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvaluationRestApplication.class, args);
+    }
+
+}
